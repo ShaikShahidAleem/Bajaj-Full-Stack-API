33dd58023dac661b9ac951da1c654704df5c4861
ShaikShahidAleem <shahidaleem070@gmail.com>
2025-08-29 12:10:46 +0530

    #

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
